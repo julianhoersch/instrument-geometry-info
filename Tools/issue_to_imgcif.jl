d17fc680f4325ca7a95a31bbd9bde8176bbff3b1
James.Hester <jxh@ansto.gov.au>
2022-06-20 19:20:26 +1000

    Fixed bug in kappa axis calculation, correct setting of principal axis
    The program erroneously assumed that the X axis was always from
    sample to goniometer, but the X axis is actually identical to the
    rotation direction of the principal axis.

--- Tools/issue_to_imgcif.jl
+++ Tools/issue_to_imgcif.jl
@@ -1,6 +1,6 @@
 # This routine takes a Github issue and produces an imgCIF fragment,
 # which is then passed to Github to create a PR
-using Downloads, JSON, Markdown, CrystalInfoFramework
+using Downloads, JSON, Markdown, CrystalInfoFramework, Printf
 
 #=====================
 
@@ -77,6 +77,11 @@ post_process(md_result) = begin
     if haskey(md_result,"Goniometer axes")
         md_result["Goniometer axes"] = parse_axis_string(md_result["Goniometer axes"])
     end
+    if haskey(md_result,"Two theta axis")
+        md_result["Two theta axis"] = md_result["Two theta axis"] == "clockwise" ? "c" : "a"
+    else
+        md_result["Two theta axis"] = missing
+    end
     return md_result
 end
 
@@ -104,20 +109,31 @@ below, so the sense of rotation is reversed.
 
 """
 make_gonio_axes(raw_info) = begin
-    n = length(raw_info["Goniometer axes"][1])
+    axes,senses = raw_info["Goniometer axes"]
+    n = length(axes)
     axis_type = fill("rotation",n)
     equip = fill("goniometer",n)
-    depends_on = Union{String,Nothing}[]
-    for i in raw_info["Goniometer axes"][1][2:end]
-        push!(depends_on,i)
-    end
-    push!(depends_on,nothing)
     kappa_axis = split(get(raw_info,"kappa","- - -"),[' ',','])[1]
+
+    # Construct axis dependency chain
+
+    depends_on = Union{String,Nothing}[]
+    append!(depends_on,axes[2:end])
+    push!(depends_on,nothing)
+
+    # Remember the principal axis direction
+
+    principal = senses[end]
+
+    # Create direction vectors
+    
     vector = Vector{Union{Missing,Real}}[]
-    for (a,d) in zip(raw_info["Goniometer axes"]...)
-        rotfac = d == "c" ? -1 : 1
+    for (a,d) in zip(axes,senses)
+        rotfac = d == principal ? 1 : -1
         if a == kappa_axis
-            push!(vector,get_kappa_info(raw_info)*rotfac)
+            kv = get_kappa_info(raw_info)
+            kv[1] *= rotfac
+            push!(vector,kv)
         else
             push!(vector,[1,0,0]*rotfac)
         end
@@ -130,26 +146,33 @@ end
 #
 #
 get_kappa_info(raw_info) = begin
-    all_axes = raw_info["Goniometer axes"]
+
+    # extract kappa information
+
+    axes,senses = raw_info["Goniometer axes"]
     kinfo = split(raw_info["kappa"],[' ',','],keepempty=false)
     if length(kinfo) == 2 push!(kinfo,"0") end
     axname = kinfo[1]
-    if !(axname in all_axes[1])
+    if !(axname in axes)
         throw(error("Kappa axis $axname not listed as a goniometer axis"))
     end
     kapang = parse(Float64,kinfo[2])
     kappoise = parse(Float64,kinfo[3])
-    # Now calculate direction of axis
-    dwn_comp = cosd(kapang)
+    
+    # Now calculate direction of axis in X-Z plane assuming
+    # rotation of kappa is same as principal axis. There is no
+    # Y component as home position is always under
+    # beam path.
+
+    up_comp = cosd(kapang)
     across_comp = sind(kapang)
-    x_comp = -1*dwn_comp
     if kappoise == 0
         #is under incident beam collimator in X-Z plane
         z_comp = -1*across_comp
     elseif kappoise == 180
         z_comp = across_comp
     end
-    return [x_comp,0.0,z_comp]
+    return [up_comp,0.0,z_comp]
 end
 
 """
@@ -165,35 +188,54 @@ end
     4. vector -> worked out from user-provided info
     5. offset -> beam centre
 
-    Note that the imgCIF X axis is always from the sample to the goniometer,
-    in particular, it does not change direction depending on the sense of
-    rotation of the goniometer.
+    
 """
 make_detector_axes(raw_info) = begin
+
+    # Insert assumed axis names and orientations
+    
     axis_id = ["two_theta","trans","detx","dety"]
     axis_type = fill("translation",4)
     axis_type[1] = "rotation"
     equip = fill("detector",4)
-    depends_on = [nothing,"two_theta","trans","trans"]
-    vector = [[missing,0,0],[0,0,-1]]
+    depends_on = [nothing,"two_theta","trans","detx"]
+
+    # Read necessary information
+    
+    principal_sense = raw_info["Goniometer axes"][2][end]
     principal = raw_info["Principal axis orientation"]
     corner = raw_info["Image orientation"]
+
+    # Adjust two theta direction
+
+    rotsense = raw_info["Two theta axis"] == principal_sense ? 1 : -1
+    vector = [[rotsense,0,0]]
+
+    # Detector translation always opposite to beam direction
+    
+    push!(vector,[0,0,-1])
+
     # Work out det_x and det_y
-    x_d,y_d = determine_detx_dety(principal,corner)
+
+    x_d,y_d = determine_detx_dety(principal,principal_sense,corner)
     push!(vector,x_d)
     push!(vector,y_d)
-    # Beam centre is unknown for now
-    offset = [[0,0,0],[0,0,missing],[missing,missing,0],[missing,missing,0]]
+    
+    # Beam centre and distance is unknown for now
+
+    offset = [[0,0,0],[0,0,missing],[missing,missing,0],[0,0,0]]
     return axis_id,axis_type,equip,depends_on,vector,offset
 end
 
 # Determine direction of detx (horizontal) and dety (vertical) in
 # imgCIF coordinates.
 
-determine_detx_dety(principal_angle,corner) = begin
+determine_detx_dety(principal_angle,principal_sense,corner) = begin
+    
     # Start with basic value and then flip as necessary
-    x_direction = [1,0,0]        # spindle is at 0, top_left origin
-    y_direction = [0,-1,0]       #
+
+    x_direction = [-1,0,0]        # spindle rotates anticlockwise at 0, top_left origin
+    y_direction = [0,1,0]       #
     if corner == "top right"
         x_direction *= -1
     elseif corner == "bottom right"
@@ -202,33 +244,68 @@ determine_detx_dety(principal_angle,corner) = begin
     elseif corner == "bottom left"
         y_direction *= -1
     end
-    if principal_angle == "90"
+
+    @debug "Before pa adjustment" x_direction y_direction
+    
+    # The direction of the principal axis flips by 180 if the sense
+    # changes
+
+    pa = principal_sense == "a" ? parse(Int64,principal_angle) : parse(Int64,principal_angle) + 180
+    if pa >= 360 pa = pa - 360 end
+
+    @debug "Principal axis direction" pa
+    
+    if pa == 90
         temp = x_direction
         x_direction = y_direction
         y_direction = -1*temp
-    elseif principal_angle == "180"
+    elseif principal_angle == 180
         x_direction *= -1
         y_direction *= -1
-    elseif principal_angle == "270"
+    elseif principal_angle == 270
         temp = x_direction
         x_direction = -1*y_direction
         y_direction = temp
     end
+
+    @debug "After pa adjustment" x_direction y_direction
+    
     return x_direction,y_direction
 end
 
+"""
+    split_vector
+
+Distribute the vectors in `vector` over CIF data names,
+formatting as integers if they are exact.
+"""
 split_vector(vector::Vector,basename,imgblock) = begin
     for i in 1:3
-        imgblock[basename*"[$i]"] = map(x->x[i],vector)
+        imgblock[basename*"[$i]"] = map(vector) do x
+            if ismissing(x[i]) x[i]
+            else
+                if round(x[i]) == x[i]
+                    @sprintf "%d" x[i]
+                else
+                    @sprintf "%1.5f" x[i]
+                end
+            end
+        end
     end
 end
 
+"""
+    describe_axes(raw_info,imgblock)
+
+Create the goniometer axes corresponding to the data in `raw_info`,
+placing the result in CIF block `imgblock`.
+"""
 describe_axes(raw_info,imgblock) = begin
        
     gon_axes = make_gonio_axes(raw_info)
     det_axes = make_detector_axes(raw_info)
     for i in 1:length(gon_axes)
-        println("$(gon_axes[i]) -- $(det_axes[i])")
+        @debug "$(gon_axes[i]) -- $(det_axes[i])"
         append!(gon_axes[i],det_axes[i])
     end
     base = "_axis."
@@ -244,6 +321,12 @@ describe_axes(raw_info,imgblock) = begin
                           base*"offset[1]",base*"offset[2]",base*"offset[3]"])
 end
 
+get_pixel_sizes(raw_info) = begin
+    both = parse.(Float64,split(raw_info["Pixel size"],","))
+    if length(both) == 1 push!(both,both[1]) end
+    return both
+end
+
 """
     describe_detector(raw_info)
 
@@ -251,14 +334,14 @@ Produce the information required for array_structure_list. Here
 we assume a rectangular detector with x horizontal, y vertical
 """
 describe_array(raw_info,imgblock) = begin
+    hor,vert = get_pixel_sizes(raw_info)
     # array structure list axis
     base = "_array_structure_list_axis."
     imgblock[base*"axis_id"] = ["detx","dety"]
     imgblock[base*"axis_set_id"] = ["1","2"]
-    imgblock[base*"displacement"] = [missing,missing]   #pixel size
-    imgblock[base*"start"] = [0,0]
+    imgblock[base*"displacement"] = [hor/2,vert/2]   #half pixel size
     create_loop!(imgblock,[base*"axis_id",base*"axis_set_id",
-                      base*"displacement",base*"start"])
+                      base*"displacement"])
     
     # array structure list
     base = "_array_structure_list."
