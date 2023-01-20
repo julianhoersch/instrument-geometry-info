15f3126048eca4348489e8b7ec4bed294b68018f
Julian Michael Hoersch <julian.hoersch@xfel.eu>
2023-01-20 14:37:45 +0100

    Add possibility to type first letter of input only, add source and gravity axes to the detector axes and calculate a first beamcentre guess.

--- Tools/imgCIF_Creator/imgCIF_Creator/command_line_interfaces/parser.py
+++ Tools/imgCIF_Creator/imgCIF_Creator/command_line_interfaces/parser.py
@@ -28,22 +28,23 @@ class CommandLineParser():
         # TODO sometimes e.g. for chi a trailing space is enough to fail the regex
         self.validation_regex = {
             'doi': None,
-            'layout': self._create_regex_from_options('layout'),
-            'facility': self._create_regex_from_options('facility'),
+            'layout': self._create_options_regex('layout'),
+            'facility': self._create_options_regex('facility'),
             'beamline': None,
             'rad_type': None,
             "model" : None,
             "location" : None,
-            "manufacturer" : self._create_regex_from_options('manufacturer'),
+            "manufacturer" : self._create_options_regex('manufacturer'),
             # 'Is updated?': r'yes|no\Z', # no effect?
             # 'Start date': r'\d{4}-\d{2}-\d{2}\Z', # does not prevent the user from putting
             # in unrealistic values, no effect?
             # 'Finish date': r'\d{4}-\d{2}-\d{2}\Z', # no effect?
-            'principal_orientation': r'\d{1,3}\Z',
+            'principal_angle': r'\d{1,3}\Z',
             'goniometer_axes': r'(?:.+,\s*(a|c))+\Z', # matches n patterns xxxxx, a|c
             'goniometer_axes' : r'((?:[^,]*,\s*(a|c),\s*)*[^,]*,\s*(a|c))\Z',
             'rotation_axis': None, # no effect?
-            "two_theta_axis" : r'(a|c|anticlockwise|clockwise)\Z', # no effect?
+            # "two_theta_sense" : r'(a|c|anticlockwise|clockwise)\Z', # no effect?
+            "two_theta_sense" : self._create_options_regex('two_theta_sense'),
             # "detector_repositioning" : None, # no effect?
             'detector_repositioning': None, # no effect? almost the same as the above
             "detector_axes" : None, #r'(?:\S*,\s*)+(\S*)\Z', # this is not exact
@@ -51,18 +52,18 @@ class CommandLineParser():
             "chi_axis" : r'.*(?:\s+\d{1,3})\Z',
             #(?:\s+\d{1,3}){1,2}\Z
             'kappa_axis': r'.*(((\s|,)(\s)*\d{1,3}){1,2})\Z', # capture something like kappa, 50, 50
-            'image_orientation': self._create_regex_from_options('image_orientation'),
-            'fast_direction': self._create_regex_from_options('fast_direction'),
+            'image_orientation': self._create_options_regex('image_orientation'),
+            'fast_direction': self._create_options_regex('fast_direction'),
             'pixel_size': r'(\d+\.?\d*\Z)|(\d+\.?\d*,\s*\d+\.?\d*\Z)', # matches either
             # one pixel dimension or both separated by a comma
-            # 'pixel_number': r'(\d+,{0,1}\s*\d+\Z)', # matches two numbers separated by
-            'pixel_number': r'(\d+(,\s*)\d+\Z)',
+            # 'array_dimension': r'(\d+,{0,1}\s*\d+\Z)', # matches two numbers separated by
+            'array_dimension': r'(\d+(,\s*)\d+\Z)',
             # a comma or a whitespace
             # 'xds.inp': None, # no effect
             'doi': None, # no effect?
             'comments': None, # no effect?
             'filename' : r'.*((\.h5)\Z|(\.cbf)\Z|(\.smv)\Z)',
-            'goniometer_rot_direction' : r'(a|c|anticlockwise|clockwise)\Z',
+            # 'goniometer_rot_direction' : self._create_options_regex('goniometer_rot_direction'), # no effect
             'frame_numbers' : r'^\d+(,\s*\d+)*$'
         }
 
@@ -79,10 +80,10 @@ class CommandLineParser():
             # 'Start date': r'\d{4}-\d{2}-\d{2}\Z', # does not prevent the user from putting
             # in unrealistic values, no effect?
             # 'Finish date': r'\d{4}-\d{2}-\d{2}\Z', # no effect?
-            'principal_orientation': True,
+            'principal_angle': True,
             'goniometer_axes': True, # matches n patterns xxxxx, a|c
             'rotation_axis': False, # no effect?
-            "two_theta_axis" : False, # no effect?
+            "two_theta_sense" : False, # no effect?
             # "detector_repositioning" : None, # no effect?
             'detector_repositioning': False, # no effect? almost the same as the above
             'detector_axes' : True,
@@ -93,11 +94,11 @@ class CommandLineParser():
             'pixel_size': True, # matches either
             # one pixel dimension or both separated by a comma
             # 'xds.inp': None, # no effect
-            'pixel_number' : True,
+            'array_dimension' : True,
             'doi': True, # no effect?
             'comments': False, # no effect?
             'filename' : True,
-            'goniometer_rot_direction' : True,
+            # 'goniometer_rot_direction' : True,
             'frame_numbers': True
         }
 
@@ -119,74 +120,17 @@ class CommandLineParser():
             print(f"\n{self.input_options[label]['label']} ({required}):")
             choices = ''
             if self.input_options[label].get('options') is not None:
-                choices = '\n Choices: ' +\
-                    ', '.join(self.input_options[label]['options'])
+                options = self.input_options[label]['options']
+                if self.input_options[label].get('abbreviate'):
+                    options = [option + f' ({option[0]})' for option in options]
+                choices = '\n Choices: ' + ', '.join(options)
             print(f"{self.input_options[label]['description']}".strip('\n') + choices)
-            self.parsed[label] = self.validated_user_input(label)
+            self.parsed[label] = self._validated_user_input(label)
 
         return self.parsed[label]
 
 
-    def validated_user_input(self, label):
-        """Request an user input and validate the input according to an apppropriate
-        regular expression.
 
-        Args:
-            label (str): the label that identifies the request
-        """
-
-        user_input = input(' >> ')
-        # validation pattern
-        if self.validation_regex.get(label) is not None:
-            pattern = re.compile(
-                self.validation_regex[label])
-            parsed_input = pattern.match(user_input)
-            if parsed_input:
-                parsed_input = parsed_input.group(0)
-        else:
-            parsed_input = user_input
-
-        # required parameter, but either regex failed or no input was made if no regex
-        # is defined
-        if self.required_information.get(label) and parsed_input in [None, '']:
-            print(' ==> Could not interpret your input correctly! This input is required, \
-please try again.')
-            parsed_input = None
-        # not required with defined regex, but no user input
-        elif not self.required_information.get(label) and user_input == '':
-            parsed_input = ''
-        # not required with defined regex, but user input
-        elif not self.required_information.get(label) and parsed_input is None:
-            print(' ==> Could not interpret your input correctly! Please try again.')
-
-        if parsed_input is not None:
-            print(f" ==> Your input was: {parsed_input}")
-
-        return parsed_input
-
-
-    def _create_regex_from_options(self, label, case_insensitive=True):
-        """Create a regular expression that matches only the options for the given
-        label.
-
-        Args:
-            label (str): the label that identifies the request
-            case_insensitive (bool, optional): Whether the regex should be case
-                insensitive. Defaults to True.
-
-        Returns:
-            regexp: regular explression formed by the options for the input label
-        """
-
-        options =  self.input_options[label].get('options')
-        if options is not None:
-            options_regex = r'('+ r'|'.join(options) + r')\Z'
-            if case_insensitive:
-                options_regex = r'(?i)' + options_regex
-        else:
-            options_regex = None
-
-        return options_regex
 
 
     def parse_axis_string(self, axis_string):
@@ -297,8 +241,8 @@ please try again.')
         return axes_dict
 
 
-    def make_detector_axes(self, goniometer_axes, principal_orientation,
-                           image_orientation, two_theta_sense):
+    def make_detector_axes(self, goniometer_axes, principal_angle,
+                           image_orientation, two_theta_sense, array_info):
         """Add information concerning the detector axes. We define our own axis names,
         with the detector distance being inserted when the data file is read. We
         choose det_x to be in the horizontal direction, and det_y to be vertical.
@@ -311,7 +255,7 @@ please try again.')
 
         Args:
             goniometer_axes (str): the goniometer axes as parsed from the user input
-            principal_orientation (int): the orientation of the principal axis in
+            principal_angle (int): the orientation of the principal axis in
                 degree
             image_orientation (str): the image orientation string, e.g. 'top left',...
             two_theta_sense (str): the sense of the two theta axis (e.g. clockwise)
@@ -321,30 +265,44 @@ please try again.')
         """
 
         # Insert assumed axis names and orientations
-        axis_id = ["two_theta", "trans", "detx", "dety"]
-        axis_type = ["translation" for _ in axis_id]
-        axis_type[0] = "rotation"
-        equip = ["detector" for _ in axis_id]
-        depends_on = ['.', "two_theta", "trans", "detx"]
+        axis_id = ["source", "gravity", "two_theta", "trans", "detx", "dety"]
+        axis_type = ['.', '.', "rotation", "translation", "translation", "translation"]
+        equip = ["source", "gravity", "detector", "detector", "detector", "detector"]
+        depends_on = ['.', '.', '.', "two_theta", "trans", "detx"]
 
-        # Read necessary information
+        # Read necessary information, last axis is first in stack
         principal_sense = goniometer_axes[1][-1]
         corner = image_orientation
 
         # Adjust two theta direction
-        rotsense = 1 if two_theta_sense == principal_sense else -1
-        vector = [[rotsense, 0, 0]]
+        if two_theta_sense in ['anticlockwise', 'a']:
+            rotsense = 1 if 'a' == principal_sense else -1
+        elif two_theta_sense in ['clockwise', 'c']:
+            rotsense = 1 if 'c' == principal_sense else -1
+        else:
+            rotsense = 1
+        # rotsense = 1 if two_theta_sense == principal_sense else -1
+        gravity = self._determine_gravity(principal_angle, principal_sense)
+
+        vector = [[0, 0, 1], gravity, [rotsense, 0, 0]]
+        # vector = [[rotsense, 0, 0]]
 
         # Detector translation always opposite to beam direction
         vector.append([0, 0, -1])
 
         # Work out det_x and det_y
-        x_d, y_d = self._determine_detx_dety(principal_orientation, principal_sense, corner)
+        beam_x, beam_y = self._calculate_beam_centre(
+            array_info['array_dimension'], array_info['pixel_size'])
+
+        x_d, y_d, x_c, y_c = self._determine_detx_dety(
+            principal_angle, principal_sense, corner, beam_x, beam_y)
+        # x_d, y_d = self._determine_detx_dety(principal_angle, principal_sense, corner)
         vector.append(x_d)
         vector.append(y_d)
 
         # TODO Beam centre is unknown for now
-        offset = [[0, 0, 0], [0, 0, 0], ['?', '?', 0], [0, 0, 0]]
+        offset = [[0, 0, 0],[0, 0, 0], [0, 0, 0], [0, 0, 0], [x_c, y_c, 0], [0, 0, 0]]
+        # offset = [[0, 0, 0], [0, 0, 0], ['?', '?', 0], [0, 0, 0]]
 
         axes_dict = {
             'axes' : axis_id,
@@ -355,6 +313,43 @@ please try again.')
             'offset' : offset,
         }
         return axes_dict
+
+    def _validated_user_input(self, label):
+        """Request an user input and validate the input according to an apppropriate
+        regular expression.
+
+        Args:
+            label (str): the label that identifies the request
+        """
+
+        user_input = input(' >> ')
+        # validation pattern
+        if self.validation_regex.get(label) is not None:
+            pattern = re.compile(
+                self.validation_regex[label])
+            parsed_input = pattern.match(user_input)
+            if parsed_input:
+                parsed_input = parsed_input.group(0)
+        else:
+            parsed_input = user_input
+
+        # required parameter, but either regex failed or no input was made if no regex
+        # is defined
+        if self.required_information.get(label) and parsed_input in [None, '']:
+            print(' ==> Could not interpret your input correctly! This input is required, \
+please try again.')
+            parsed_input = None
+        # not required with defined regex, but no user input
+        elif not self.required_information.get(label) and user_input == '':
+            parsed_input = ''
+        # not required with defined regex, but user input
+        elif not self.required_information.get(label) and parsed_input is None:
+            print(' ==> Could not interpret your input correctly! Please try again.')
+
+        if parsed_input is not None:
+            print(f" ==> Your input was: {parsed_input}")
+
+        return parsed_input
 
 
     def _make_kappa_vector(self, kappa_info):
@@ -422,7 +417,36 @@ please try again.')
         return list(np.dot(chi_rot, chi_beam_dir))
 
 
-    def _determine_detx_dety(self, principal_angle, principal_sense, corner):
+    def _determine_gravity(self, principal_angle, principal_sense):
+        """Determine the gravity vector.
+
+        Args:
+            principal_angle (str): the angle of the principal axis in degree
+            principal_sense (str): the sense of rotation of the principal axis
+
+        Returns:
+            list: the gavity vector
+        """
+
+        angle = int(principal_angle) if principal_sense == "a" \
+            else int(principal_angle) + 180
+
+        if angle >= 360:
+            angle = angle - 360
+
+        if angle == 0:
+            gravity = [0, 1, 0]  #spindle at 3 o'clock, rotating anticlockwise
+        elif angle == 90:
+            gravity = [1, 0, 0]
+        elif angle == 180:
+            gravity = [0, -1, 0]
+        else:
+            gravity = [-1, 0, 0]
+
+        return gravity
+
+
+    def _determine_detx_dety(self, principal_angle, principal_sense, corner, beam_x, beam_y):
         """Determine direction of detx (horizontal) and dety (vertical) in
         imgCIF coordinates.
 
@@ -430,24 +454,36 @@ please try again.')
             principal_angle (str): the principal angle
             principal_sense (str): the principal sense of the goniometer axes
             corner (str): the orientation of the first pixel (e.g. 'top right')
+            beam_x (str): the beam center in x direction in mm
+            beam_y (str): the beam center in y direction in mm
 
         Returns:
-            _type_: _description_
+            x_direction (list): the vector for the detector x direction
+            y_direction (list): the vector for the detector y direction
+            x_centre (float): the beamcentre in x direction
+            y_centre (float): the beamcentre in y direction
+
         """
 
         # Start with basic value and then flip as necessary
         x_direction = [-1, 0, 0] # spindle rotates anticlockwise at 0, top_left origin
         y_direction = [0, 1, 0]
+        x_centre = beam_x
+        y_centre = -1 * beam_y
 
         if corner == "top right":
             x_direction = [i * -1 for i in x_direction]
+            x_centre = -1 * beam_x
 
         elif corner == "bottom right":
             x_direction = [i * -1 for i in x_direction]
             y_direction = [i * -1 for i in y_direction]
+            x_centre *= -1
+            y_centre *= -1
 
         elif corner == "bottom left":
             y_direction = [i * -1 for i in y_direction]
+            y_centre *= -1
 
         # The direction of the principal axis flips by 180 if the sense changes
         angle = int(principal_angle) if principal_sense == "a" \
@@ -458,14 +494,70 @@ please try again.')
 
         if angle == 90:
             temp = x_direction
+            temp_centre = x_centre
             x_direction = y_direction
+            x_centre = y_centre
             y_direction = [i * -1 for i in temp] #-1*temp
+            y_centre = temp_centre
         elif angle == 180:
             x_direction = [i * -1 for i in x_direction]
             y_direction = [i * -1 for i in y_direction]
+            x_centre *= -1
+            y_centre *= -1
         elif angle == 270:
             temp = x_direction
+            temp_centre = x_centre
             x_direction = [i * -1 for i in y_direction] #-1*y_direction
+            x_centre = -1 * y_centre #-1*y_direction
             y_direction = temp
+            y_centre = temp_centre
 
-        return x_direction, y_direction
+        return x_direction, y_direction, x_centre, y_centre
+
+
+    def _create_options_regex(self, label, case_insensitive=True):
+        """Create a regular expression that matches only the options for the given
+        label.
+
+        Args:
+            label (str): the label that identifies the request
+            case_insensitive (bool, optional): Whether the regex should be case
+                insensitive. Defaults to True.
+
+        Returns:
+            regexp: regular expression formed by the options for the input label
+        """
+        options =  self.input_options[label].get('options')
+        if options is not None:
+            options_regex = r'|'.join(options)
+            if self.input_options[label].get('abbreviate'):
+                first_letters = [option[0] for option in options]
+                letters_regex = r'|'.join(first_letters)
+                options_regex += r'|' + letters_regex
+            options_regex = r'(' + options_regex + r')\Z'
+
+            if case_insensitive:
+                options_regex = r'(?i)' + options_regex
+        else:
+            options_regex = None
+
+        return options_regex
+
+
+    def _calculate_beam_centre(self, array_dimension, pixel_size):
+        """The default beam centre is at the centre of the detector. We must
+        indicate this position in mm with the correct signs.
+
+        Args:
+            array_dimension (tuple): a tuple with the x and y dimension of the pixels
+            pixel_size (tuple): a tuple with the pixel sizes in x an y direction
+
+        Returns:
+            tuple: the x and y beam centre in mm
+        """
+
+        nx, ny = array_dimension
+        # nx, ny = parse.(Int64,split(raw_info["Number of pixels"],","))
+        pix_x, pix_y = pixel_size
+
+        return float(pix_x) * float(nx)/2, float(pix_y) * float(ny)/2
